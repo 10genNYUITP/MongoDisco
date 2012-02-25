e31f9d512a4c735575dbb65bc2b84b4a3e30d9e5
Changle Wang <clwangnyu@gmail.com>
2012-02-24 23:26:49 -0500

    add skelton code for splitter

--- MongoSplitter.py
+++ MongoSplitter.py
@@ -7,12 +7,49 @@ Author: NYU ITP Team
 Description: Will calculate splits for a given collection/database
 and store/return them in MongoSplit objects
 '''
-
+from pymongo import Connection, uri_parser
+import logging
 
 def calculate_splits(config):
     """reads config to find out what type of split to perform"""
-    pass
-
+    #pass
+	uri = "mongodb://localhost/test.in" #config.getInputURI()
+	uri_info = uri_parser.parse_uri(uri)
+	
+	host = uri_info['nodelist'][0][0]
+	port = uri_info['nodelist'][0][1]
+	database_name = uri_info['database']
+	collection_name = uri_info['collection']
+	
+	connection = Connection(uri)
+	db = connection[databaseName]
+	stats = db.command("collstats", collection_name)
+	
+	isSharded = False if "sharded" not in stats else stats["sharded"]
+	useShards = False #config.canReadSplitsFromShards()
+	useChunks = False #config.isShardChunkedSplittingEnabled()
+	laveOk = True #config.canReadSplitsFromSecondary()
+	
+	logging.info(" Calculate Splits Code ... Use Shards? " + useShards + ", Use Chunks? " + useChunks + "; Collection Sharded? " + isSharded);
+	
+	if config.createInputSplits():
+		logging.info( "Creation of Input Splits is enabled." )
+		if isSharded and (useShards or useChunks):
+			if useShards and useChunks:
+				logging.warn( "Combining 'use chunks' and 'read from shards directly' can have unexpected & erratic behavior in a live system due to chunk migrations. " );
+			
+			logging.info( "Sharding mode calculation entering." );
+			return calculate_sharded_splits( config, useShards, useChunks, slaveOk, uri, mongo );
+			
+			else: # perfectly ok for sharded setups to run with a normally calculated split. May even be more efficient for some cases
+				logging.info( "Using Unsharded Split mode (Calculating multiple splits though)" );
+				return calculate_unsharded_splits( config, slaveOk, uri, coll );
+	           
+	else:
+		logging.info( "Creation of Input Splits is disabled; Non-Split mode calculation entering." );
+		return calculate_single_split( config );
+		
+		
 def calculate_unsharded_splits(config, etc):
     """@todo: Docstring for calculate_unsharded_splits
 
