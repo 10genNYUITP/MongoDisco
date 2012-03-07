a8f8bc35eccabb542eba97e48035e428d6fbdf85
aflock <andrewbflockhart@gmail.com>
2012-03-06 22:11:59 -0500

    moar codez

--- scheme_mongo.py
+++ scheme_mongo.py
@@ -1,5 +1,26 @@
 import pymongo
+from pymongo import Connection, uri_parser
 
-def open(url, task=None):
+def open(url=None, task=None):
     #parses a mongodb uri and returns the database??
+    #say url is mongo://dbname.collectionName
+
+    uri = url if url else "mongodb://localhost/test.in"
+    #config.getInputURI()
+    uri_info = uri_parser.parse_uri(uri)
+
+    print uri_info
+    return
+    host = uri_info['nodelist'][0][0]
+    port = uri_info['nodelist'][0][1]
+    database_name = uri_info['database']
+    collection_name = uri_info['collection']
+    #connect to mongodb
     return mongodb
+
+def input_stream(size, url, params):
+    return open()
+
+if __name__ == '__main__':
+    open()
+
