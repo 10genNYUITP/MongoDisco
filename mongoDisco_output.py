bb49af74aa71597fd3801e87f9fdeabd7d3466a7
Changle Wang <changlewang@ubuntu.(none)>
2012-03-20 17:41:21 -0400

    pushed mongo output stream into mongodb_io.py and wrote the io test

--- mongoDisco_output.py
+++ mongoDisco_output.py
@@ -25,22 +25,30 @@ class MongoDBoutput(object):
                 port = node[1]
 
         self.conn = Connection(host=host,port=port)
+
         username = uri_info['username'] or username
         password = uri_info['password'] or password
+
+        if username:
+            if not self.conn[db].authenticate(username,password):
+                raise ConfigurationError("authentication failed")
+
         self.coll = self.conn[db][col]
         #self.conn = mongoUtil.getConnection(uri)
         #self.coll = mongoUtil.getCollection(uri)
         self.stream = stream
         self.params = params
 
-        self.key_name = "key"#Should read from config
-        self.value_name = "value"
+        self.key_name = "word"#Should read from config
+        self.value_name = "count"
 
 
     def add(self,key,val):
         result_dict = {}
-        key, val = str(key), str(val)
-        result_dict[key] = val
+        #key, val = str(key), str(val)
+        #result_dict[key] = val
+        result_dict[self.key_name] = key
+        result_dict[self.value_name] = val
         self.coll.insert(result_dict)
 
     def close(self):
--- test_io.py
+++ test_io.py
@@ -1,27 +1,23 @@
 from disco.core import Job, result_iterator
-from mongoDisco_output import MongoDBoutput
-from disco.worker.classic.func import task_output_stream
 from mongodb_io import mongodb_output_stream
+from disco.worker.classic.func import task_output_stream
 
-def map(line, params):
-    for word in line.split():
-        yield word,1
+import logging
+
+def map(record, params):
+    logging.info("%s" % record.get('_id'))
+    yield record.get('name', "NoName"), 1
 
 def reduce(iter, params):
     from disco.util import kvgroup
     for word, counts in kvgroup(sorted(iter)):
         yield word, sum(counts)
 
-'''
-def mongodb_output(stream,partition,url,params):
-    return mongoDisco_output.MongoDBoutput(stream,params)
-'''
-
 if __name__ == '__main__':
-
-    job = Job().run(input=["raw://123 it it it hi hi mi"],
+    job = Job().run(input=["mongodb://localhost/test.modforty"],
             map=map,
             reduce=reduce,
             reduce_output_stream=mongodb_output_stream)
 
     job.wait(show=True)
+
