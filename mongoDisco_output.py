984919a7c08cfe35d041763021e1ba619e024ce7
Changle Wang <changlewang@ubuntu.(none)>
2012-03-20 16:23:24 -0400

    change module import to make output stream work

--- mongoDisco_output.py
+++ mongoDisco_output.py
@@ -3,13 +3,33 @@ class MongoDBoutput(object):
     '''Output stream for mongoDB 
     '''
     def __init__(self,stream,params):
-        #import pymongo
-        #from pymongo import Connection,uri_parser
-        import mongoUtil
+        import pymongo
+        from pymongo import Connection,uri_parser
         self.uri = "mongodb://localhost/test.out"
         #self.uri = url if url else "mongodb://localhost/test.out"
-        self.conn = mongoUtil.getConnection(uri)
-        self.coll = mongoUtil.getCollection(uri)
+
+        uri_info = uri_parser.parse_uri(self.uri)
+        nodes = set()
+        host = None
+        port = None
+        username = None
+        password = None
+        db = uri_info['database']
+        col = uri_info['collection']
+
+        nodes.update(uri_info['nodelist'])
+
+        if len(nodes) == 1:
+            for node in nodes:
+                host = node[0]
+                port = node[1]
+
+        self.conn = Connection(host=host,port=port)
+        username = uri_info['username'] or username
+        password = uri_info['password'] or password
+        self.coll = self.conn[db][col]
+        #self.conn = mongoUtil.getConnection(uri)
+        #self.coll = mongoUtil.getCollection(uri)
         self.stream = stream
         self.params = params
 
@@ -19,16 +39,15 @@ class MongoDBoutput(object):
 
     def add(self,key,val):
         result_dict = {}
-        k, v = str(k), str(v)
+        key, val = str(key), str(val)
         result_dict[key] = val
         self.coll.insert(result_dict)
 
     def close(self):
         self.conn.close()
 
-def mongodb_output(stream,partition,url,params):
-    from mongoDisco_output import MongoDBoutput
-    return MongoDBoutput(stream,params)
+#def mongodb_output(stream,partition,url,params):
+#    return MongoDBoutput(stream,params)
 
 
 
