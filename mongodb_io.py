182d76f0d73ab15fc13686042f9096922e3bef59
Changle Wang <changlewang@ubuntu.(none)>
2012-03-21 02:05:57 -0400

    add mongodb input stream to mongodb_io

--- mongodb_io.py
+++ mongodb_io.py
@@ -1,7 +1,9 @@
 from mongoDisco_output import MongoDBoutput
 from disco.worker.classic.func import task_output_stream
+from disco.schemes.scheme_mongodb import input_stream
 
 def mongodb_output(stream,partition,url,params):
     return mongoDisco_output.MongoDBoutput(stream,params)
 
 mongodb_output_stream = (task_output_stream,mongodb_output)
+mongodb_input_stream = (input_stream,)
