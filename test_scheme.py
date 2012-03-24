e8fcb7f99c6358a58696552230a4f65c1f8d68d1
Ramya Ballamudi <rb2502@nyu.edu>
2012-03-24 00:50:45 -0400

    to work on splits

--- test_scheme.py
+++ test_scheme.py
@@ -1,12 +1,15 @@
+from disco.util import kvgroup
 import unittest
-import scheme_mongo
+import scheme_mongodb
 class TestScheme(unittest.TestCase):
     def runTest(self):
-        mongo_uri = "mongodb://localhost/test.in"
-        wrapper = scheme_mongo.open(mongo_uri)
+        mongo_uri = "mongodb://localhost/test.modforty"
+        wrapper = scheme_mongodb.open(mongo_uri)
         assert wrapper
         for result in wrapper:
             print result
+            for key in result:
+                print result[key]
             assert result
             break
 
