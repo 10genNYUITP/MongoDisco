b65fcf656ce89957c08f3caf9806a21d2c3cb4ad
Andrew Flockhart <andrewbflockhart@gmail.com>
2012-03-27 02:33:59 +0000

    test_io.py passes (hell yeah)

--- test_io.py
+++ test_io.py
@@ -1,6 +1,7 @@
 from disco.core import Job, result_iterator
 from mongodb_io import mongodb_output_stream,mongodb_input_stream
 from disco.schemes.scheme_mongodb import input_stream
+#from app.scheme_mongodb import input_stream
 from disco.worker.classic.func import task_output_stream
 from app.MongoSplitter import calculate_splits as do_split
 
@@ -8,9 +9,9 @@ import logging
 
 config = {
         "db_name": "test",
-        "collection_name": "modforty",
+        "collection_name": "in",
         "splitSize": 1, #MB
-        "inputURI": "mongodb://localhost/test.modforty",
+        "inputURI": "mongodb://localhost/test.in",
         "createInputSplits": True,
         "splitKey": {'_id' : 1},
         }
@@ -29,23 +30,22 @@ if __name__ == '__main__':
 #    logging.getLogger().setLevel(logging.DEBUG)
     #mongodb_input_stream = (input_stream,)
     input_s_splits =  do_split(config)
-    #print input_s_splits
-    
-    for i in input_s_splits:
-        i.format_uri_with_query()
+    print input_s_splits
+
     #split = input_s_splits[0]
-    
+
     #back_dump = split.format_uri_with_query()
-    #input_urls = [split.format_uri_with_query() for split in input_s_splits]
-	#use scheme to 
+    input_urls = [split.format_uri_with_query() for split in input_s_splits]
+    print input_urls
+	#use scheme to
 
- #   job = Job().run(
+    job = Job().run(
   #          input=["mongodb://localhost/test.modforty"],
-            #input= input_urls,
-   #         map=map,
-    #        reduce=reduce,
-     #       map_input_stream = mongodb_input_stream,
-      #      reduce_output_stream=mongodb_output_stream)
+            input= input_urls,
+            map=map,
+            reduce=reduce,
+            map_input_stream = mongodb_input_stream,
+            reduce_output_stream=mongodb_output_stream)
 
-    #job.wait(show=True)
+    job.wait(show=True)
 
