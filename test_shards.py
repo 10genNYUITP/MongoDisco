00f3dc3e52fbc30d6353db267429860a0c566461
Changle Wang <clwangnyu@gmail.com>
2012-04-17 18:24:29 -0400

    make sharding works!

--- test_shards.py
+++ test_shards.py
@@ -1,6 +1,12 @@
 from app import MongoSplitter as MS
+import time
+from disco.core import Job,result_iterator
+from mongodb_io import mongodb_output_stream,mongodb_input_stream
+from disco.schemes.scheme_mongodb import input_stream
+from app.MongoSplitter import calculate_splits as do_split
 import pymongo
 from pymongo import Connection
+from app.mongoUtil import getCollection
 
 
 '''
@@ -20,9 +26,61 @@ print original_URI_4,MS.get_new_URI(original_URI_4,new_URI,False)
 
 config = {
         "inputURI":"mongodb://localhost/test.people",
-        "slaveOk":"true",
-        "useShards":True
+        "slaveOk":True,
+        "useShards":True,
+        "createInputSplits":True,
+        "useChunks":True
         }
+
+
+def map(record,params):
+    age = record.get('age',0)/10
+    yield age, 1
+
+def reduce(iter,params):
+    from disco.util import kvgroup
+    for age,counts in kvgroup(sorted(iter)):
+        yield age,sum(counts)
+
+def test_traditional_way():
+    start = time.clock()
+
+    col = getCollection(config['inputURI'])
+    count = {}
+    cur = col.find()
+    for row in cur:
+        age = row['age']/10
+        if age in count:
+            count[age] += 1
+        else:
+            count[age] = 1
+
+    end = time.clock()
+    print "Time used: ", end-start
+    for key in count:
+        print key,count[key]
+        
+
+if __name__ == '__main__':
+
+
+    job = Job().run(
+            input = do_split(config),
+            map = map,
+            reduce = reduce,
+            map_input_stream = mongodb_input_stream,
+            )
+    
+
+    totalCount = 0;
+    for age, count in result_iterator(job.wait(show=True)):
+        range = str(age*10)+" -- "+str(age*10+9)
+        totalCount += count
+        print range, count
+
+    print "total count : ", totalCount
+
+    test_traditional_way()
 
 def test_chunks():
 
@@ -37,15 +95,8 @@ def test_chunks():
     print "splits count: %d" %len(splits)
     count = 0
     for split in splits:
-        print split.format_uri_with_query()
-        connection = Connection(split.inputURI)
-        db= connection[database]
-        col = db[collection]
-        cursor = col.find(split.query)
-        print cursor.count()
-        count = count + cursor.count()
+        for row in split.cursor:
+            count +=1
+        
     print count
-
-test_chunks()
-
 
