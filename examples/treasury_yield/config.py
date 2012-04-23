6c4e36844e657465f91f11cbf9c984658d11e0e3
aflock <andrewbflockhart@gmail.com>
2012-04-23 12:58:40 -0400

    add twitter example

---
+++ examples/treasury_yield/config.py
@@ -0,0 +1,17 @@
+'''
+File: config.py
+Author: AFlock
+Description: A sample configuration file for the mongo-disco adapter
+Use: from config import config
+'''
+
+config = {
+        #which collection to use for input to the job
+        "inputURI":"mongodb://localhost/yield_historical.in",
+        "slaveOk":True,
+        "useShards":True,
+        #Split the data (almost always want True)
+        "createInputSplits":True,
+        #Use mongo chunks as splits
+        "useChunks":True
+        }
