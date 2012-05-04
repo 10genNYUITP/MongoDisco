14e029254b00a2269b36b5c15fdfbe7e8c6bf5c6
Changle Wang <clwangnyu@gmail.com>
2012-05-03 22:06:14 -0400

    add DiscoJob

--- app/MongoInputSplit.py
+++ app/MongoInputSplit.py
@@ -9,7 +9,7 @@ Description: Holds the specification for an individual
 import sys, os, logging
 import json
 from bson import json_util,son
-from mongo_util import getConnection
+from mongo_util import getConnection,getCollection
 from pymongo import Connection, uri_parser
 from pymongo.uri_parser import (_partition,
                                 _rpartition,
@@ -36,85 +36,10 @@ class MongoInputSplit():
         self.timeout = timeout
 
 
-        #self.cursor = self.get_cursor()
-        #TODO this ^^  feels weird and very un-pythonic to me... -AF 2/27/12
-        #replacing for now with below code VV
-
-        #Assign cursor
-        uri_info = uri_parser.parse_uri(inputURI)
-        host = uri_info['nodelist'][0][0]
-        port = uri_info['nodelist'][0][1]
-        database_name = uri_info['database']
-        collection_name = uri_info['collection']
-
-        #connection = getConnection(inputURI)
-        connection = Connection(inputURI)
-        db = connection[database_name]
-        collection = db[collection_name]
         logging.info("LOOK HERE")
         logging.info("%s" % query)
         logging.info("%s" % fields)
-        self.cursor = collection.find(query,fields) #.sort(sortSpec) doesn't work?
-                                               # @todo support limit/skip --CW
 
-        '''
-        if self.noTimeout:
-            # TODO should be something else? blank for now
-            self.cursor.add_option()
-        '''
-
-
-    def write(self, out):
-        """@todo: Docstring for write
-
-        :out: @todo
-        :returns: void
-        """
-        pass
-
-    def read_fields(self, input):
-        """read each field sequentially?
-        see http://bit.ly/y2TjIj for corresponding f(n)
-
-        :in: @todo
-        :returns: void
-        """
-        pass
-
-    def get_cursor(self):
-        """Do a find operation
-
-        :returns: a cursor with the split's query
-        """
-
-
-        ''' @todo Encasuplate these stuff into MongoConfigUtil
-            call like MongoConfigUtil.getCollection(URI)
-        '''
-        uri_info = uri_parser.parse_uri(uri)
-        host = uri_info['nodelist'][0][0]
-        port = uri_info['nodelist'][0][1]
-        database_name = uri_info['database']
-        collection_name = uri_info['collection']
-
-        connection = getConnection(uri)
-        db = connection[database_name]
-        collection = db[collection_name]
-        self.cursor = collection.find(query,fields) #.sort(sortSpec) doesn't work?
-                                               # @todo support limit/skip --CW
-        if self.noTimeout:
-            self.cursor.add_option()
-
-            # self.cursor.slaveOk() read from the slave(s) by using slaveOk
-            # find how to do it in python --CW
-
-
-
-
-    # NOT INCLUDING: getters/setters  for all the data members (this is Python, not Java ^_^)
-
-    def __str__(self):
-        return self.cursor
 
     def format_uri_with_query(self):
         """
@@ -124,37 +49,6 @@ class MongoInputSplit():
         mongodb://local/test.in?query=<json of query>&limit=234&skip=293
         """
         base = self.inputURI
-        #query is a bson object
-        #- need to convert to a list of tuples and jsonify
-        #o_l = []
-        #o_l = []
-        #query contains: "$query", "min", "max
-
-        #o_l.append(("$query", self.query['$query']))
-        '''
-        o_q = self.query['$query']
-        #o_l.update({"$query": self.query['$query']})
-        if self.query.get("$min"):
-            oid = str(self.query['$min'])
-            o_q.update({'$min': oid})
-            #o_l.append(("$min", oid))
-        if self.query.get("$max"):
-            oid = str(self.query['$max'])
-            o_q.update({'$max': oid})
-            #o_l.append(("$max", oid))
-
-        o_l.append(("$query", o_q))
-        print 'o_l: ', o_l
-        #need to rearrange any dict with an ObjectID to look live  VV
-        # '{"_id": {"$oid": "4edebd262ae5e93b41000000"}}'
-
-
-
-        base += '?query='
-        base += json.dumps(self.query, default=json_util.default)
-        #TODO add options of
-
-        '''
         queryObj = son.SON()
         queryObj['inputURI'] = self.inputURI
         queryObj['keyField'] = self.keyField
@@ -165,7 +59,6 @@ class MongoInputSplit():
         queryObj['skip'] = self.skip
         queryObj['timeout'] = self.timeout
 
-        print
         str = json.dumps(queryObj,default=json_util.default)
         print str
         return str
