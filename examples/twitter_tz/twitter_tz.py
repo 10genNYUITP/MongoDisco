9f92660b203c6a60446b9ba70014fda7816809da
Ramya Ballamudi <rb2502@nyu.edu>
2012-05-03 22:43:06 -0400

    twitter example pep8

--- examples/twitter_tz/twitter_tz.py
+++ examples/twitter_tz/twitter_tz.py
@@ -4,9 +4,11 @@
 '''
 File: twitter_tz.py
 Author: AFlock
-Description: Given a database of tweets, aggregate number of tweets per timezone
+Description: Given a database of tweets, aggregate number of tweets per time
+zone
 Note: before running this job fill a mongodb with tweets by running :
-curl https://stream.twitter.com/1/statuses/sample.json -u<user>:<pass> | mongoimport -c twitter
+curl https://stream.twitter.com/1/statuses/sample.json -u<user>:<pass> \
+| mongoimport -c twitter
 
 
 '''
@@ -16,24 +18,25 @@ from mongodb_io        import mongodb_input_stream
 from app.MongoSplitter import calculate_splits
 
 config = {
-        "inputURI":"mongodb://localhost/twitter",
-        "createInputSplits" : True,
-        }
+        "inputURI": "mongodb://localhost/twitter",
+        "createInputSplits": True}
+
 
 def map(tweet, params):
     yield tweet['user'].get('time_zone', "unlisted"), 1
+
 
 def reduce(iter, params):
     from disco.util import kvgroup
     for zone, number in kvgroup(sorted(iter)):
         yield zone, sum(number)
 
+
 if __name__ == '__main__':
-    job = Job().run(input=calculate_splits(config) ,
-            map = map,
-            reduce = reduce,
-            map_input_stream = mongodb_input_stream
-            )
+    job = Job().run(input=calculate_splits(config),
+            map=map,
+            reduce=reduce,
+            map_input_stream=mongodb_input_stream)
 
     for tz, count in result_iterator(job.wait(show=True)):
         print "TZ : %s :: Count : %s" % (tz, count)
