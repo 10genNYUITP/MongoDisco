03949191c57163c5458d0970e2d0b3f203c8b9b2
Dan Crosta <dcrosta@late.am>
2012-05-09 19:30:37 -0400

    make MongoDisco packageable

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distribute_setup import use_setuptools
+use_setuptools('0.6.15')
+
+from setuptools import setup
+setup(
+    name='MongoDisco',
+    version='0.8',
+    url='https://github.com/10genNYUITP/MongoDisco',
+    license='Apache Software License, Version 2.0',
+    packages=['mongodisco'],
+    install_requires=[
+        'pymongo >= 2.1'
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 3 - Alpha',
+    ],
+)
