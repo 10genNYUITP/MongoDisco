0e01115dbb4374e5e61996b6f6eb0eed744608c3
Dan Crosta <dcrosta@late.am>
2012-05-19 12:52:28 -0400

    Apache 2.0 License

--- mongodisco/job.py
+++ mongodisco/job.py
@@ -1,5 +1,17 @@
-#!/usr/bin/env python
-# encoding: utf-8
+# Copyright 2012 10gen, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 '''
 File: DiscoJob.py
 Author: NYU ITP team
@@ -14,24 +26,36 @@ from splitter import calculate_splits as do_split
 
 class DiscoJob():
 
+    DEFAULT_CONFIG = {
+        "job_output_key" : "_id",
+        "job_output_value" : "value",
+        "input_uri" : "mongodb://localhost/test.in",
+        "output_uri" : "mongodb://localhost/test.out",
+        "print_to_stdout": False,
+        "job_wait":True,
+        "split_size" : 8,
+        "split_key" : {"_id" : 1},
+        "create_input_splits" : True,
+        "splits_use_shards" : False,
+        "splits_use_chunks" : True,
+        "slaveok" : False,
+        "limit" : 0,
+        "skip" : 0,
+        "inputKey" : None,
+        "sort" : None,
+        "timeout" : False,
+        "fields" : None,
+        "query" : {}
+    }
 
     def __init__(self,config,map,reduce):
-        import config_util
-
-        self.config = config_util.config
-        #if the user doesn't specify output, print to stdout
-        if not config.get('output_uri') and not config.get('print_to_stdout'):
-            config['print_to_stdout'] = True
-
-        for item in config:
-            self.config[item] = config[item]
+        self.config = DiscoJob.DEFAULT_CONFIG.copy()
+        self.config.update(config)
 
         self.map = map
         self.reduce = reduce
         self.job = Job()
-        self.params = Params()
-        for key in self.config:
-            self.params.__dict__[key] = self.config[key]
+        self.params = Params(**self.config)
 
     def run(self):
 
